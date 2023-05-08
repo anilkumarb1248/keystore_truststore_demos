535fbe866251458f40df39cfd9f2c57f4ec606cf
anilkumarb1248 <anilkumarb1248@gmail.com>
2023-05-08 08:41:20 +0530

    Twoway SSL working

--- mutual-handshake-with-keytool-gent-jks-files/demo-client/demo-client/src/main/java/com/client/demo/controller/ClientController.java
+++ mutual-handshake-with-keytool-gent-jks-files/demo-client/demo-client/src/main/java/com/client/demo/controller/ClientController.java
@@ -1,8 +1,11 @@
 package com.client.demo.controller;
 
 import com.client.demo.clients.ServerClient;
+import com.client.demo.dto.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +25,11 @@ public class ClientController {
         String clientData = "Response from the client";
         String serverData = serverClient.getDataFromServer();
         return "Client Data: " + clientData + ", /n Server Data: " + serverData;
+    }
+
+    @GetMapping("/post")
+    public String addData(){
+        Student student = new Student(100, "Anil");
+        return serverClient.addData(student);
     }
 }
--- mutual-handshake-with-keytool-gent-jks-files/demo-server/demo-server/src/main/java/com/server/demo/controller/ServerController.java
+++ mutual-handshake-with-keytool-gent-jks-files/demo-server/demo-server/src/main/java/com/server/demo/controller/ServerController.java
@@ -1,6 +1,9 @@
 package com.server.demo.controller;
 
+import com.server.demo.dto.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +14,10 @@ public class ServerController {
     @GetMapping("/data")
     public String getData(){
         return "Returning data in secure way from server";
+    }
+
+    @PostMapping("/data")
+    public String addData(@RequestBody Student student){
+        return "Adding Student with id: " + student.getId() + ", name: " + student.getName() + " in server";
     }
 }
